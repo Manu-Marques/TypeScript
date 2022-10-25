e9ad97f3317bfc06d383eaca6faaf248e727dc3e
Manu <le.marques.emanuel@gmail.com>
2022-10-25 17:39:17 +0200

    continue to learn ts

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,35 @@
+// const a : string = "Hello World";
+
+// const b : number = 42;
+
+// const c : boolean = true;
+
+// const d : null = null;
+
+// const e : any[] = ["Hello", 42, true];
+
+// const f : { [key: string]: string } = { "key": "value" };
+
+// const user : {firstname : string, lastname : string, age : number} = { firstname : "john", lastname : "doe", age : 42 };
+
+// const g : Function =  (e: MouseEvent) : void => {
+
+// }
+
+// const h : Function = (id : number) : void => {
+
+// }
+
 const compteur = document.querySelector("#compteur");
 let i = 0;
 
-const increment = (e) => {
+const increment = (e: Event) => {
+    e.preventDefault();
     i++;
-    compteur.querySelector("span").innerText = i.toString();
-    }
+  const span = compteur?.querySelector("span")
+    if (span) {
+        span.innerText = i.toString();
+        }
+}
 
-compteur.addEventListener("click", increment);
+compteur?.addEventListener("click", increment);
